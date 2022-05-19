22d4f258f1934ddf73f7ffda96c58ae7d31de169
Lennard <27106448+SirWalross@users.noreply.github.com>
2022-05-19 19:18:01 +0200

    Cleanup arduino projects

--- DHT11_Mehrere/DHT11_Mehrere.ino
+++ DHT11_Mehrere/DHT11_Mehrere.ino
@@ -1,58 +1,36 @@
 
-#include <DHT.h>  // DHT-Bibliothek einbinden
+#include <DHT.h>
 
-#define DHT1_Pin 6   // Datenpin des DHT11(1) ist Pin 6
-#define DHT2_Pin 8   // Datenpin des DHT11(2) ist Pin 8
-#define DHT3_Pin 10  // Datenpin des DHT11(3) ist Pin 10
-#define DHT4_Pin 13  // Datenpin des DHT11(3) ist Pin 13
+#define DHT1_Pin 6
+#define DHT2_Pin 8
+#define DHT3_Pin 10
+#define DHT4_Pin 13
 
-// Je nachdem, ob Sensoren vom Typ DHT11 oder DHT 22 benutzt werden die entsprechende "DHT_Typ"-Zeile benutzen
-// Hier wird der DHT11 benutzt
+#define DHT_Typ DHT11
 
-#define DHT_Typ DHT11  // DHT 11 als Sensortyp festlegen
-//#define DHT_Typ DHT22              // DHT 22 als Sensortyp festlegen
-
-DHT dht1(DHT1_Pin, DHT_Typ);  // Sensor 1 initialisieren
-DHT dht2(DHT2_Pin, DHT_Typ);  // Sensor 2 initialisieren
-DHT dht3(DHT3_Pin, DHT_Typ);  // Sensor 3 initialisieren
-DHT dht4(DHT4_Pin, DHT_Typ);  // Sensor 4 initialisieren
+DHT dths[] = {DHT(DHT1_Pin, DHT_Typ), DHT(DHT2_Pin, DHT_Typ), DHT(DHT3_Pin, DHT_Typ), DHT(DHT4_Pin, DHT_Typ)};
 
 void setup() {
     Serial.begin(9600);
-    dht1.begin();  // Sensor 1 starten
-    dht2.begin();  // Sensor 2 starten
-    dht3.begin();  // Sensor 3 starten
-    dht4.begin();  // Sensor 4 starten
+
+    for (int i = 0; i < sizeof(dhts); i++) {
+        dths[i].begin();
+    }
 }
 
 void loop() {
     if (Serial.available()) {
         Serial.read();
-        // Da das Display nur 2 Zeilen hat, werden die 3 Sensoren nacheinander ausgelesen und angezeigt
 
-        float h1 = dht1.readHumidity();     // Auslesen der Luftfeuchtigkeit (Sensor 1)
-        float t1 = dht1.readTemperature();  // Auslesen der Temperatur (Sensor 2)
-
-        // Serial.print("T1   ");
-        Serial.println(t1, 2);
         delay(10);
 
-        float h2 = dht2.readHumidity();     // Auslesen der Luftfeuchtigkeit (Sensor 2)
-        float t2 = dht2.readTemperature();  // Auslesen der Temperatur (Sensor 2)
-        // Serial.print("T2   ");
-        Serial.println(t2, 2);
-        delay(10);
+        Serial.println(2);  // for identification
 
-        float h3 = dht3.readHumidity();     // Auslesen der Luftfeuchtigkeit (Sensor 2)
-        float t3 = dht3.readTemperature();  // Auslesen der Temperatur (Sensor 2)
-        // Serial.print("T3   ");
-        Serial.println(t3, 2);
-        delay(10);
+        for (int i = 0; i < sizeof(dths) / sizeof(*dths); i++) {
+            float temp = dhts[i].readTemperature();
 
-        float h4 = dht4.readHumidity();
-        float t4 = dht4.readTemperature();
-        // Serial.print("T4   ");
-        Serial.println(t4, 2);
-        delay(10);
+            Serial.println(temp);
+            delay(10);
+        }
     }
 }
